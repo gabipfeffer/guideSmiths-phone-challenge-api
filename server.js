b539e0d81f2b2a805a6cea3523b009bc7cd433d6
Gabriel Pfeffer <gabrielpfeffer@gmail.com>
2020-02-11 17:22:59 +0100

    created /phones endpoint and get method for phone list and phone by id

--- server.js
+++ server.js
@@ -3,4 +3,20 @@ const express = require('express');
 const port = 3005;
 const app = express();
 
+const phones = require('./phones');
+
+app.get('/phones', (req, res) => {
+  !phones
+    ? res.status(400).json('unable to get phone list')
+    : res.status(200).json(phones);
+});
+
+app.get('/phones/:idPhone', (req, res) => {
+  const { idPhone } = req.params;
+
+  !phones
+    ? res.status(400).json('unable to get phone by id')
+    : res.json(phones.filter(phone => phone.id.toString() === idPhone));
+});
+
 app.listen(port, console.log(`Server is listening on port ${port}`));
